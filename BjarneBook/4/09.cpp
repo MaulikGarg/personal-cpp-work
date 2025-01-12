3adafc1f3c9a84670205866e7c8c87cd7ac278f8
Maulik Garg <maulikgarg678@gmail.com>
2025-01-12 16:56:08 +0530

    added exercises

--- BjarneBook/4/09.cpp
+++ BjarneBook/4/09.cpp
@@ -6,75 +6,26 @@
 
 using namespace std;
 
-void iGiveErrors(string s)
+bool isPrime(int number)
 {
-    cerr << "error: " << s << '\n';
-    exit(1);
+    for (int i = 2; i * i <= number; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
 }
-
-
 
 int main()
 {
-    vector<char> computerMoves;
-    cout << "How many moves should the computer have? : ";
-    int input{};
-    cin >> input;
-    if (input < 1)
-        iGiveErrors("It must have 1 move!");
+    vector<int> primes;
+    constexpr int kMaxNumber {100};
 
-    cout << "Enter computer moves(r/s/p): ";
-    for (int i = 0; i < input; i++)
+    for (int i = 2; i < kMaxNumber; i++)
     {
-        char move{};
-        cin >> move;
-        if ((move != 'r') && (move != 's') && (move != 'p'))
-        {
-            iGiveErrors("Invalid input");
-        }
-        computerMoves.push_back(move);
+        if(isPrime(i)) primes.push_back(i);
     }
 
-    for (int counter = 0; counter < computerMoves.size(); counter++)
-    {
-        cout << "Enter your move!\n move: ";
-        char move{};
-        cin >> move;
-        if ((move != 'r') && (move != 's') && (move != 'p'))
-        {
-            iGiveErrors("Invalid input");
-        }
-
-        switch (move)
-        {
-        case 'r':
-            if (computerMoves[counter] == 'p')
-                cout << "Computer won!\n";
-            else if (computerMoves[counter] == 's')
-                cout << "You won!\n";
-            else
-                cout << "Tie!";
-            break;
-        case 's':
-            if (computerMoves[counter] == 'r')
-                cout << "Computer won!\n";
-            else if (computerMoves[counter] == 'p')
-                cout << "You won!\n";
-            else
-                cout << "Tie!";
-            break;
-        case 'p':
-            if (computerMoves[counter] == 's')
-                cout << "Computer won!\n";
-            else if (computerMoves[counter] == 'r')
-                cout << "You won!\n";
-            else
-                cout << "Tie!";
-            break;
-
-        default:
-            break;
-        }
-    }
+    for(int i : primes) cout << i << " ";
     return 0;
 }
