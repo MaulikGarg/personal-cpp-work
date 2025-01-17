6e8258f944eb598a182f14f12d33155d424eb6e9
Maulik Garg <maulikgarg678@gmail.com>
2025-01-17 22:52:22 +0530

    updated minigame

--- PersonalProjects/guessTheNumber.cpp
+++ PersonalProjects/guessTheNumber.cpp
@@ -18,6 +18,8 @@ namespace gameparameters
 using namespace std;
 using namespace gameparameters;
 
+auto getNumberSet() -> set<char>; //returns the randomized n numbers
+void printSet(set<char> funcSetTemporary);
 void err(string errorstring);
 void gameplay();
 bool clearFailedExtraction();
@@ -42,13 +44,8 @@ int main()
 
 void gameplay()
 {
-    // original computer set to store the generated random numbers
-    set<char> numbers;
-    while (numbers.size() < numberCount)
-    {
-        numbers.insert(static_cast<char>(Random::get(0, 9) + '0'));
-    }
-
+    set<char> numbers = getNumberSet();
+    
     while (true)
     {
         cout << "enter your 4 diff integer digit guess: ";
@@ -58,6 +55,7 @@ void gameplay()
         {
             char input{};
             cin >> input;
+            if(input == 't') printSet(numbers); //only for testing
             userInput.insert(input);
         }
 
@@ -70,14 +68,16 @@ void gameplay()
 
         for (int i = 0; i < numberCount; i++)
         {
-            /* //checks if the value at current iterator is equal to 
+            /* //checks if the value at current iterator is equal to
             the value at the same iterator position for the generated set,
             if not, checks if the vector even has a generated number.*/
 
-            if (*it_user == *it_generated) correctDigit++;
-            else if (userInput.count(*it_generated)) correctPosition++;
+            if (*it_user == *it_generated)
+                correctDigit++;
+            else if (userInput.count(*it_generated))
+                correctPosition++;
 
-            //incrementing the iterator so it moves to the next element
+            // incrementing the iterator so it moves to the next element
             it_user++;
             it_generated++;
         }
@@ -87,45 +87,25 @@ void gameplay()
             cout << "you win!";
             break;
         }
-        
+
         // printing input
-        for (char i : userInput)
-            cout << i << ' ';
+        printSet(userInput);
         cout << "\nRight number and position: " << correctPosition << " \nRight number wrong position: " << correctDigit << '\n';
     }
 }
 
-/*error checking field*/
-void err(string errorstring)
+auto getNumberSet() -> set<char>
 {
-    cerr << errorstring;
-    exit(1);
-}
-
-// function to use max console limit and clear the console thus the input buffer
-
-void ignoreLine()
-{
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-// if(clearFailedExtraction()) then it will change cin state and clear bad input. else it will just clear bad input anyway.
-bool clearFailedExtraction()
-{
-    // Check for failed extraction
-    if (!std::cin) // If the previous extraction failed
+    // original computer set to store the generated random numbers
+    set<char> numbers;
+    while (numbers.size() < numberCount)
     {
-        if (std::cin.eof()) // If the stream was closed
-        {
-            std::exit(0); // Shut down the program now
-        }
-
-        // Let's handle the failure
-        std::cin.clear(); // Put us back in 'normal' operation mode
-        ignoreLine();     // And remove the bad input
-
-        return true;
+        numbers.insert(static_cast<char>(Random::get(0, 9) + '0'));
     }
+    return numbers;
+}
 
-    return false;
+void printSet(set<char> funcSetTemporary){
+    for(char i: funcSetTemporary)
+        cout << i << ' ';
 }
