1d23e1b96fd3201d69611275655bfe0955326375
Maulik Garg <maulikgarg678@gmail.com>
2025-01-26 07:12:42 +0530

    added leap year

--- BjarneBook3ed/8/5/date.cpp
+++ BjarneBook3ed/8/5/date.cpp
@@ -44,12 +44,22 @@ public:
 
     //increment the day by 1
     void incrementDay(int n) { d += n; }
+
+    //checks if the current year is a leap year, return true if it is
+    bool isLeapYear();
 };
 
 //checks if the date is valid by checking month and date availability
 void Date::is_valid(Month mm, int dd){
     if(dd > 31 || dd < 1) exit(1);
     if(static_cast<int>(mm) > 12 || static_cast<int>(mm) < 1) exit(1);
+}
+
+//return true if the object is a leap year.
+bool Date::isLeapYear(){
+    if(y.value%400 == 0) return true;
+    if(y.value%4 == 0 && y.value%100 != 0) return true;
+    return false;
 }
 
 std::ostream &operator<<(std::ostream &os, const Date &date)
