3c572a586579cee78c0689721c4b834db837d9c0
Maulik Garg <maulikgarg678@gmail.com>
2025-01-29 08:04:17 +0530

    drill update

--- BjarneBook3ed/9/00_drill.cpp
+++ BjarneBook3ed/9/00_drill.cpp
@@ -3,9 +3,19 @@ using namespace std;
 
 int main()
 {
-  int birth_year {2005};
-  cout << "Dec\t" << "Hex\t" << "Oct\t" << '\n';
-  cout << birth_year << '\t'<< hex << birth_year << '\t' <<  oct << birth_year << '\n';
+  // int birth_year {2005};
+  // cout << "Dec\t" << "Hex\t" << "Oct\t" << '\n';
+  // cout << birth_year << '\t'<< hex << birth_year << '\t' <<  oct << birth_year << '\n';
+
+
+  // int a,b,c,d;
+  // cin >> a >>oct >> b >> hex >> c >> d;
+  // cout << a << '\t'<< b << '\t'<< c << '\t'<< d << '\n';
+
+  // double num{1234567.89};
+  // cout << num << '\t' << fixed << num << scientific << '\t' << num << '\n'; 
+
+  
 
   return 0;
 }
