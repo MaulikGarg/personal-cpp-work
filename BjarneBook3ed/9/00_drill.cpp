cbdbbca4e7845ba1641e18a6e20c205f80da4f4a
Maulik Garg <maulikgarg678@gmail.com>
2025-01-29 07:51:55 +0530

    start drill

---
+++ BjarneBook3ed/9/00_drill.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int birth_year {2005};
+  cout << "Dec\t" << "Hex\t" << "Oct\t" << '\n';
+  cout << birth_year << '\t'<< hex << birth_year << '\t' <<  oct << birth_year << '\n';
+
+  return 0;
+}
