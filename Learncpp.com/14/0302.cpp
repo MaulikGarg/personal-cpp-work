e80bfaa0ef528fa4db79cc07cbada7b556a53d36
Maulik Garg <maulikgarg678@gmail.com>
2025-01-31 06:22:14 +0530

    added 03/02

--- Learncpp.com/14/0302.cpp
+++ Learncpp.com/14/0302.cpp
@@ -6,6 +6,9 @@ struct IntPair{
     void print(){
         std::cout << "Pair(" << num1 << ',' << num2 << ")\n";
     }
+    bool isEqual(IntPair compared){
+        return num1==compared.num1 && num2==compared.num2;
+    }
 };
 
 int main()
@@ -18,6 +21,9 @@ int main()
 
 	std::cout << "p2: ";
 	p2.print();
+
+    std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
 
 	return 0;
 }
