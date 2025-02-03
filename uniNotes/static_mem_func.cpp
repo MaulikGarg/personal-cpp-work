fb7398f3c6dfddfaf959acd26536359650675348
Maulik Garg <maulikgarg678@gmail.com>
2025-02-03 21:10:16 +0530

    updated uni notes

---
+++ uniNotes/static_mem_func.cpp
@@ -0,0 +1,21 @@
+//static_mem_func.cpp <- filename
+#include <iostream>
+using namespace std;
+
+class MyClass {
+private:
+     static int count;  // Static data member declaration
+public:
+    MyClass() {
+        count++;  // constructor: Increment the count when an object is created
+    }
+    static int getCount() {return count;}
+};
+
+// Initialize the static data member outside the class because non const
+int MyClass::count = 1;
+
+int main() {
+    cout << MyClass::getCount(); //prints 1
+    return 0;
+}
