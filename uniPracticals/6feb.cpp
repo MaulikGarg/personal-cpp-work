91b404d97b24b7b3fb19a9b1538ff5b7284c2e10
Maulik Garg <maulikgarg678@gmail.com>
2025-02-11 18:45:46 +0530

    adjustments for conversion

--- uniPracticals/6feb.cpp
+++ uniPracticals/6feb.cpp
@@ -1,23 +1,27 @@
-#include <../libraries/mylib.h>  //for printing error
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
+void printError(const std::string& msg) {
+  std::cerr << "\n" << std::string(msg.length(), '-') + "\n" + msg + "\n" + std::string(msg.length(), '-') + "\n";
+  exit(1);
+}
+
 class Matrix {
   vector<vector<int>> matrix;
 
  public:
-  Matrix(int r, int c) : matrix(r, vector<int>(c)) {}
+  Matrix(size_t r, size_t c) : matrix(r, vector<int>(c)) {}
   Matrix();
 
   Matrix operator*(const Matrix& m) {
     if (matrix.empty() || m.matrix.empty() || matrix[0].size() != m.matrix.size()) printError("Undefined multiplication.");
     Matrix result(matrix.size(), m.matrix[0].size());
-    for (int i = 0; i < matrix.size(); ++i)
-      for (int j = 0; j < m.matrix[0].size(); ++j)
-        for (int k = 0; k < m.matrix.size(); ++k)
+    for (size_t i = 0; i < matrix.size(); ++i)
+      for (size_t j = 0; j < m.matrix[0].size(); ++j)
+        for (size_t k = 0; k < m.matrix.size(); ++k)
           result.matrix[i][j] += matrix[i][k] * m.matrix[k][j];
     return result;
   }
