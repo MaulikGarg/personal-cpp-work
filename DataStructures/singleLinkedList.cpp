b5da920f0cd211080af0b26fd39407680d9c430d
Maulik Garg <maulikgarg678@gmail.com>
2025-03-18 15:54:03 +0530

    add documentation and refactor

--- DataStructures/singleLinkedList.cpp
+++ DataStructures/singleLinkedList.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
 
+// a standard linked list node object with a value and a next pointer
 template <typename T>
 struct Node {
-  T m_data{};
-  Node<T>* m_next{nullptr};
+  T m_data{};                // the data inside a node
+  Node<T>* m_next{nullptr};  // the pointer to the next node
   Node(const T& data) : m_data{data}, m_next{nullptr} {}
   Node(const T& data, Node<T>* ptr) : m_data{data}, m_next{ptr} {}
 };
 
 template <typename T>
 class LinkedList {
-  Node<T>* m_head{nullptr};
-  Node<T>* m_tail{nullptr};
+  Node<T>* m_head{nullptr};  // pointer to the first element
+  Node<T>* m_tail{nullptr};  // pointer to the last element
   int m_size{};
 
  public:
+  // constructors
   LinkedList() {}
   ~LinkedList();
-  void addFirst(const T& data);
-  void addLast(const T& data);
-  void addAtIndex(const T& data, int index);
-  void removeAtIndex(int index);
-  void removeValue(const T& value);
 
+  // standard functions
+  void addFirst(const T& data);  // add data to the start of the list
+  void addLast(const T& data);   // add data to the end of the list
+  void addAtIndex(const T& data, int index);  // add data at a specific index
+  void removeAtIndex(int index);     // remove data from a specific index
+  void removeValue(const T& value);  // remove all occurences of a value
+
+  // overloads
   template <typename U>
   friend std::ostream& operator<<(std::ostream& os, const LinkedList<U>& list);
+
+  // getters
+  int getSize() const { return m_size; }
+  const T& getFirst() const {
+    if (m_head) return m_head->m_data;
+    else throw std::runtime_error("Exception in getFirst(): Cannot get element from empty list.\n");
+  }
+  const T& getLast() const {
+    if (m_head) return m_tail->m_data;
+    else throw std::runtime_error("Exception in getLast(): Cannot get element from empty list.\n");
+  }
+
 };
 
 template <typename T>
@@ -41,7 +58,7 @@ template <typename T>
 void LinkedList<T>::addFirst(const T& data) {
   Node<T>* newNode = new Node{data};
 
-  if (!m_head)
+  if (!m_head)  // empty list case
     m_head = m_tail = newNode;
   else {
     newNode->m_next = m_head;
@@ -55,7 +72,7 @@ template <typename T>
 void LinkedList<T>::addLast(const T& data) {
   Node<T>* newNode = new Node{data};
 
-  if (!m_head)
+  if (!m_head)  // empty list case
     m_head = m_tail = newNode;
   else {
     m_tail->m_next = newNode;
@@ -68,7 +85,7 @@ void LinkedList<T>::addLast(const T& data) {
 template <typename T>
 void LinkedList<T>::addAtIndex(const T& data, int index) {
   if (index < 0 || index > m_size)
-    throw std::runtime_error("Invalid Index in addAtIndex()");
+    throw std::runtime_error("Exception in addAtIndex(): Invalid Index\n");
 
   // if we are adding at the start
   if (!index) {
@@ -95,23 +112,23 @@ void LinkedList<T>::addAtIndex(const T& data, int index) {
 
 template <typename T>
 void LinkedList<T>::removeAtIndex(int index) {
-  if (!m_head) return;
+  if (!m_head) return;  // empty list case
 
   // size is always => 0, we cant delete non existing Node
   if (index < 0 || index >= m_size)
-    throw std::runtime_error("Invalid Index in removeAtIndex()");
+    throw std::runtime_error("Exception in removeAtIndex(): Invalid Index\n");
 
   if (index == 0) {
     Node<T>* temp{m_head};
     m_head = m_head->m_next;
     delete temp;
-    if (!m_head) m_tail = nullptr;
+    if (!m_head) m_tail = nullptr;  // if the list is now empty
     m_size--;
     return;
   }
 
   // prev reaches the node behind the to be deleted one
-  Node<T>* prev{m_head};
+  Node<T>* prev{m_head};  // pointer to node behind current index
   for (int i = 0; i < index - 1; i++) {
     prev = prev->m_next;
   }
@@ -126,15 +143,17 @@ void LinkedList<T>::removeAtIndex(int index) {
 
 template <typename T>
 void LinkedList<T>::removeValue(const T& value) {
-  if (!m_head) return;
+  if (!m_head) return;  // empty list case
 
+  // dummy node at index = -1
   Node<T>* dummy = new Node<T>{T{}, m_head};
-  Node<T>* current = dummy;
+  Node<T>* current = dummy;  // the traversal pointer
 
   while (current->m_next) {
     if (current->m_next->m_data == value) {
       Node<T>* temp = current->m_next;
       current->m_next = temp->m_next;
+      // if we are deleting the last element
       if (temp == m_tail) m_tail = current;
       delete temp;
       m_size--;
@@ -144,11 +163,13 @@ void LinkedList<T>::removeValue(const T& value) {
   }
 
   m_head = dummy->m_next;
+  if(!m_head) m_tail = nullptr;
   delete dummy;
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
+  if(!list.m_head) return os;
   Node<T>* current{list.m_head};
   while (current) {
     os << current->m_data << " -> ";
@@ -156,21 +177,4 @@ std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
   }
   os << "end!\n";
   return os;
-}
-
-int main() {
-  LinkedList<int> list;
-  try {
-    list.addFirst(1);
-    list.addFirst(2);
-    list.addLast(3);
-    std::cout << list;
-    list.addAtIndex(4, 2);
-    std::cout << list;
-    list.addAtIndex(5, 1);
-    list.addAtIndex(6, 2);
-  } catch (const std::runtime_error& exception) {
-    std::cout << exception.what() << '\n';
-  }
-  std::cout << list;
 }
