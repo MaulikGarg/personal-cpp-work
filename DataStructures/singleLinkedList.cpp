eb260b2c4d0e781030330938ae50ab93c2309319
Maulik Garg <maulikgarg678@gmail.com>
2025-03-18 18:18:54 +0530

    nest node in class

--- DataStructures/singleLinkedList.cpp
+++ DataStructures/singleLinkedList.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 
-// a standard linked list node object with a value and a next pointer
-template <typename T>
-struct Node {
-  T m_data{};                // the data inside a node
-  Node<T>* m_next{nullptr};  // the pointer to the next node
-  Node(const T& data) : m_data{data}, m_next{nullptr} {}
-  Node(const T& data, Node<T>* ptr) : m_data{data}, m_next{ptr} {}
-};
-
 template <typename T>
 class LinkedList {
-  Node<T>* m_head{nullptr};  // pointer to the first element
-  Node<T>* m_tail{nullptr};  // pointer to the last element
+
+  struct Node {
+    T m_data{};                // the data inside a node
+    Node* m_next{nullptr};  // the pointer to the next node
+    Node(const T& data) : m_data{data}, m_next{nullptr} {}
+    Node(const T& data, Node* ptr) : m_data{data}, m_next{ptr} {}
+  };
+
+  Node* m_head{nullptr};  // pointer to the first element
+  Node* m_tail{nullptr};  // pointer to the last element
   int m_size{};
 
  public:
@@ -46,9 +45,9 @@ class LinkedList {
 
 template <typename T>
 LinkedList<T>::~LinkedList() {
-  Node<T>* traverse{m_head};
+  Node* traverse{m_head};
   while (traverse) {
-    Node<T>* temp = traverse;
+    Node* temp = traverse;
     traverse = traverse->m_next;
     delete temp;
   }
@@ -56,7 +55,7 @@ LinkedList<T>::~LinkedList() {
 
 template <typename T>
 void LinkedList<T>::addFirst(const T& data) {
-  Node<T>* newNode = new Node{data};
+  Node* newNode = new Node{data};
 
   if (!m_head)  // empty list case
     m_head = m_tail = newNode;
@@ -70,7 +69,7 @@ void LinkedList<T>::addFirst(const T& data) {
 
 template <typename T>
 void LinkedList<T>::addLast(const T& data) {
-  Node<T>* newNode = new Node{data};
+  Node* newNode = new Node{data};
 
   if (!m_head)  // empty list case
     m_head = m_tail = newNode;
@@ -98,8 +97,8 @@ void LinkedList<T>::addAtIndex(const T& data, int index) {
     return;
   }
 
-  Node<T>* newNode = new Node{data};
-  Node<T>* current{m_head};
+  Node* newNode = new Node{data};
+  Node* current{m_head};
   // current should stop 1 before index
   for (int i = 0; i < index - 1; i++) {
     current = current->m_next;
@@ -119,7 +118,7 @@ void LinkedList<T>::removeAtIndex(int index) {
     throw std::runtime_error("Exception in removeAtIndex(): Invalid Index\n");
 
   if (index == 0) {
-    Node<T>* temp{m_head};
+    Node* temp{m_head};
     m_head = m_head->m_next;
     delete temp;
     if (!m_head) m_tail = nullptr;  // if the list is now empty
@@ -128,11 +127,11 @@ void LinkedList<T>::removeAtIndex(int index) {
   }
 
   // prev reaches the node behind the to be deleted one
-  Node<T>* prev{m_head};  // pointer to node behind current index
+  Node* prev{m_head};  // pointer to node behind current index
   for (int i = 0; i < index - 1; i++) {
     prev = prev->m_next;
   }
-  Node<T>* temp{prev->m_next};
+  Node* temp{prev->m_next};
   prev->m_next = temp->m_next;
   delete temp;
   // if prev has become that tail
@@ -146,12 +145,12 @@ void LinkedList<T>::removeValue(const T& value) {
   if (!m_head) return;  // empty list case
 
   // dummy node at index = -1
-  Node<T>* dummy = new Node<T>{T{}, m_head};
-  Node<T>* current = dummy;  // the traversal pointer
+  Node* dummy = new Node{T{}, m_head};
+  Node* current = dummy;  // the traversal pointer
 
   while (current->m_next) {
     if (current->m_next->m_data == value) {
-      Node<T>* temp = current->m_next;
+      Node* temp = current->m_next;
       current->m_next = temp->m_next;
       // if we are deleting the last element
       if (temp == m_tail) m_tail = current;
@@ -161,7 +160,7 @@ void LinkedList<T>::removeValue(const T& value) {
       current = current->m_next;
     }
   }
-
+  
   m_head = dummy->m_next;
   if(!m_head) m_tail = nullptr;
   delete dummy;
@@ -170,7 +169,7 @@ void LinkedList<T>::removeValue(const T& value) {
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
   if(!list.m_head) return os;
-  Node<T>* current{list.m_head};
+  typename LinkedList<T>::Node* current{list.m_head};
   while (current) {
     os << current->m_data << " -> ";
     current = current->m_next;
