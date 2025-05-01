c0f17c735dad5e9838419362d13cecaaf9eda3fe
Maulik Garg <maulikgarg678@gmail.com>
2025-05-01 11:55:46 +0530

    adjust and refine

--- DataStructures/simpleBinaryTreeLinked.cpp
+++ DataStructures/simpleBinaryTreeLinked.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+/*----- MAIN CLASS ----*/
 template <typename T>
 class BinaryTree {
+  /*-
+  -- NODE STRUCT
+  -*/
   // simple element struct with pointers to left right nodes
+
   struct Node {
     T mm_val{};
     Node* mm_left{nullptr};
@@ -13,17 +18,29 @@ class BinaryTree {
         : mm_val{val}, mm_left{l}, mm_right{r} {}
   };
 
+  /*-
+  -- STRUCT END
+  -*/
+
  public:
-  BinaryTree(); // constructor, inserts into root node
+  BinaryTree();  // constructor, inserts into root node
   ~BinaryTree();
-  void insert(Node* node); // adds to a node 
+  void insert(Node* node);  // adds to a node
+
+  // delete the copy functions
+  BinaryTree(const BinaryTree&) = delete;
+  BinaryTree operator=(const BinaryTree&) = delete;
 
  private:
   Node* m_root{nullptr};
   int m_size{};
-  T insertPrompt();
+  T insertPrompt();        // basic prompt asking node value and returning it
+  void clear(Node* node);  // deletes a specific node and all its children
 };
 
+/*--- class end---*/
+
+/* public functions */
 template <typename T>
 BinaryTree<T>::BinaryTree() {
   m_root = new Node{insertPrompt()};
@@ -32,22 +49,15 @@ BinaryTree<T>::BinaryTree() {
 }
 
 template <typename T>
-BinaryTree<T>::~BinaryTree(){
-  
-}
-
-template <typename T>
-T BinaryTree<T>::insertPrompt() {
-  std::cout << "Enter the node value: ";
-  T value;
-  std::cin >> value;
-  return value;
+BinaryTree<T>::~BinaryTree() {
+  clear(m_root);
 }
 
 template <typename T>
 void BinaryTree<T>::insert(Node* node) {
   char reply;
 
+  // asks if insert to left then ask if return to right
   std::cout << "Do you want to insert to the left of " << node->mm_val
             << " ?(y/n)\n>";
   std::cin >> reply;
@@ -65,4 +75,22 @@ void BinaryTree<T>::insert(Node* node) {
     insert(node->mm_right);
     ++m_size;
   }
+}
+
+/* private helper functions */
+
+template <typename T>
+T BinaryTree<T>::insertPrompt() {
+  std::cout << "Enter the node value: ";
+  T value;
+  std::cin >> value;
+  return value;
+}
+
+template <typename T>
+void BinaryTree<T>::clear(Node* node) {
+  if (!node) return;
+  clear(node->mm_left);
+  clear(node->mm_right);
+  delete node;
 }
