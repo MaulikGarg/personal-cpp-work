5c244c5ca683822e8d1bc18b2836c1785b526dee
maulik <maulikgarg678@gmail.com>
2025-10-06 17:17:13 +0530

    fix size update bug

--- DataStructures/simpleBSTlinked.cpp
+++ DataStructures/simpleBSTlinked.cpp
@@ -10,10 +10,9 @@ Add a constraint so that only comparable types can be selected for the template.
 
 /*--- primary tree class ---*/
 template <typename T>
-class BinarySearchTree{
-
+class BinarySearchTree {
   /*--- node with a value and two child pointers ---*/
-  struct Node{
+  struct Node {
     T mm_data{};
     Node* mm_left{nullptr};
     Node* mm_right{nullptr};
@@ -23,73 +22,82 @@ class BinarySearchTree{
   /*--- node struct end ---*/
 
   // primary BST functions
-  public:
-    BinarySearchTree(); // no value constructor
-    BinarySearchTree(T value); // constructor -> inserts into root
-    ~BinarySearchTree(); // destructor -> deletes all nodes
-    void insert(T value); // adds into the BST after finding the appropriate location, ignores if repeat
-    
+ public:
+  BinarySearchTree();         // no value constructor
+  BinarySearchTree(T value);  // constructor -> inserts into root
+  ~BinarySearchTree();        // destructor -> deletes all nodes
+  void insert(T value);       // adds into the BST after finding the appropriate
+                              // location, ignores if repeat
+  Node* getNodeByValue();     // get a Node's pointer by its value
 
-    // disable shallow pointer copying
-    BinarySearchTree(const BinarySearchTree&) = delete;
-    BinarySearchTree operator=(const BinarySearchTree&) = delete;
+  // disable shallow pointer copying
+  BinarySearchTree(const BinarySearchTree&) = delete;
+  BinarySearchTree operator=(const BinarySearchTree&) = delete;
 
-  private:
-  Node* m_root {};
-  int m_size {};
-  void deleteNode(Node* node); // delete a specific node
-  void insert(Node* node, T value); // insert into a specific node
-
+ private:
+  Node* m_root{};
+  int m_size{};
+  void deleteNode(Node* node);       // delete a specific node
+  void insert(Node* node, T value);  // insert into a specific node
 };
 /*--- primary class end ---*/
 
 /*--- public functions ---*/
 
 template <typename T>
-BinarySearchTree<T>::BinarySearchTree(T value){
+BinarySearchTree<T>::BinarySearchTree(T value) {
   m_root = new Node(value);
+  m_size++;
 }
 
 template <typename T>
-BinarySearchTree<T>::~BinarySearchTree(){
+BinarySearchTree<T>::~BinarySearchTree() {
   deleteNode(m_root);
 }
 
 template <typename T>
-void BinarySearchTree<T>::insert(T value){
+void BinarySearchTree<T>::insert(T value) {
   // if no root then this is root
-  if(!m_root){
+  if (!m_root) {
     m_root = new Node(value);
-  } 
-  else insert(m_root, value);
+    m_size++;
+  } else
+    insert(m_root, value);
 }
-
 
 /*--- public functions end ---*/
 
 /*--- private functions start ---*/
 
 template <typename T>
-void BinarySearchTree<T>::deleteNode(Node* node){
-  if(!node) return;
+void BinarySearchTree<T>::deleteNode(Node* node) {
+  if (!node) return;
   // first delete the node's children then the node
   deleteNode(node->mm_left);
   deleteNode(node->mm_right);
   delete node;
+  m_size--;
 }
 
 template <typename T>
-void BinarySearchTree<T>::insert(Node* node, T value){
-  
-  // check if it comes to the left or the right
-  if(node->mm_data > value){
-    if(node->mm_left) insert(node->mm_left, value); // check if left exists, if not, make it.
-    else node->mm_left = new Node(value);
+void BinarySearchTree<T>::insert(Node* node, T value) {
+  // first check the position then check if node exists.
+  if (node->mm_data > value) {
+    if (node->mm_left)
+      insert(node->mm_left, value);
+    else {
+      node->mm_left = new Node(value);
+      m_size++;
+    }
   }
 
-  else if (node->mm_data < value){
-    if(node->mm_right) insert(node->mm_right, value); // check if right exists, if not, make it.
-    else node->mm_right = new Node(value);
+  else if (node->mm_data < value) {
+    if (node->mm_right)
+      insert(node->mm_right, value);
+    else {
+      node->mm_right = new Node(value);
+      m_size++;
+    }
   }
 
   // if control reaches here, that means its equal, we ignore.
